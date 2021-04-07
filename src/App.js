7d82c6a7f852c688158e8079c08969bec6f8cfe6
ADMIN\Admin <nguyenphilongkma@gmail.com>
2021-04-07 14:01:24 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
+  const [total, setTotal] = useState(1);
+
+  const handlePlus = () => {
+    setTotal(total + 1);
+  }
+
+  const handleMinus = () => {
+    if(total > 0){
+      setTotal(total - 1);
+    }
+  }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <button onClick = {handleMinus} className="minus">
+            <span>
+                <i className="fas fa-minus"></i>
+            </span>
+        </button>
+        <div className="total">
+            {total}
+        </div>
+        <button onClick = {handlePlus} className="plus">
+            <i className="fas fa-plus"></i>
+        </button>
+    </div>
     </div>
   );
 }
